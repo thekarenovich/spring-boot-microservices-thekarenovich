e1415c18d6a0776b825ae0f25d38b41f0cbcd40e
thekarenovich <thekarenovich@yandex.ru>
2023-10-13 22:39:16 +0300

    added mail delivery module

--- README.md
+++
@@ -1,3 +0,0 @@
-# Microservices
-
-![alt text](diagram.png)
---
+++ email-sender/src/main/java/com/thekarenovich/emailsender/service/EmailService.java
@@ -0,0 +1,7 @@
+package com.thekarenovich.emailsender.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface EmailService  {
+    String sendMail(MultipartFile[] file, String to, String[] cc, String subject, String body);
+}
